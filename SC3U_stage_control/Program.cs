8b48474d29b6f63f63d6f6b5f8843d889402ae30
Melody Chien <53807242+MstrisMoigara@users.noreply.github.com>
2026-03-13 17:20:33 -0700

    Add files via upload
    C# Stage Project Folder

---
+++ SC3U_stage_control/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using SC3Us; //try this
+
+namespace CSharp
+{
+    static class Program // static class cannot be instantiated and can only contain static members
+    {
+        /// <summary>
+        /// 应用程序的主入口点。
+        /// </summary>
+        [STAThread] //attribute that indicates COM threading model for the application is single-threaded apartment (STA)
+        static void Main()
+        {
+            System.Diagnostics.Debug.WriteLine("Printing from Program.cs main");
+
+            // Diagnostics to confirm which EXE is running.
+            System.Diagnostics.Debug.WriteLine("BaseDirectory: " + AppDomain.CurrentDomain.BaseDirectory);
+
+            System.Diagnostics.Debug.WriteLine("Process MainModule: " + System.Diagnostics.Process.GetCurrentProcess().MainModule.FileName);
+
+            // Optional visible confirmation when not attached todebugger
+            MessageBox.Show("Running EXE:\n" + Application.ExecutablePath);
+            System.Diagnostics.Debug.WriteLine("Main ran");
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
